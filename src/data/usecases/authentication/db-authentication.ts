43fe128bc585d9acdb089f74c8ab97553853a326
Weverton Couto <wevertoncristais@gmail.com>
2022-06-24 19:38:25 -0300

    feat: ensure DbAuthentication calls UpdateAcessTokenRepository with correct values

--- src/data/usecases/authentication/db-authentication.ts
+++ src/data/usecases/authentication/db-authentication.ts
@@ -2,16 +2,19 @@ import { Authentication, AuthenticationModel } from '../../../domain/usecases/au
 import { HashComparer } from '../../protocols/criptography/hash-comparer'
 import { TokenGenerator } from '../../protocols/criptography/token-generator'
 import { LoadAccountByEmailRepository } from '../../protocols/db/load-account-by-repository'
+import { UpdateAccessTokenRepository } from '../../protocols/db/update-acess-token-repository'
 
 export class DbAuthentication implements Authentication {
   private readonly loadAccountByEmailRepository: LoadAccountByEmailRepository
   private readonly hashComparer: HashComparer
   private readonly tokenGenerator: TokenGenerator
+  private readonly updateAcessTokenRepository: UpdateAccessTokenRepository
 
-  constructor (loadAccountByEmailRepository: LoadAccountByEmailRepository, hashComparer: HashComparer, tokenGenerator: TokenGenerator) {
+  constructor (loadAccountByEmailRepository: LoadAccountByEmailRepository, hashComparer: HashComparer, tokenGenerator: TokenGenerator, updateAcessTokenRepository: UpdateAccessTokenRepository) {
     this.loadAccountByEmailRepository = loadAccountByEmailRepository
     this.hashComparer = hashComparer
     this.tokenGenerator = tokenGenerator
+    this.updateAcessTokenRepository = updateAcessTokenRepository
   }
 
   async auth (authentication: AuthenticationModel): Promise<string | null> {
@@ -20,6 +23,7 @@ export class DbAuthentication implements Authentication {
     const isValid = await this.hashComparer.compare(authentication.password, account.password)
     if (isValid) {
       const accessToken = await this.tokenGenerator.generate(account.id)
+      await this.updateAcessTokenRepository.update(account.id, accessToken)
       return accessToken
     }
     return null
